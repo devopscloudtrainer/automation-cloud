6fd70d2929e74d6cf35e61a46d6dafc74c12f714
devopscloudtrainer <119645417+devopscloudtrainer@users.noreply.github.com>
2025-12-15 10:55:11 +0530

    Ticket: 09822 added transaction.sh for automation

---
+++ transaction.sh
@@ -0,0 +1,13 @@
+Commands:
+-----------------------------------------------------------------------------------------------------------------------------
+git branch                            // list all local branches
+git branch -a                         // list all local and remote branches
+git branch branch_name                // create an branch
+git checkout branch_name              // switch an branch
+git checkout -b branch_name           // create and switch branch
+git branch -D branch_name
+
+  master  - M1
+    |
+  develop - M1 D1 
+    | 
