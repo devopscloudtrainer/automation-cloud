44697ad1c16744bef4bf815556c93648c1cd113c
GoudSagar <goud.sagar.t@gmail.com>
2025-12-15 05:27:21 +0000

    Ticket-876544 Updated transaction.sh file

--- transaction.sh
+++ transaction.sh
@@ -11,3 +11,19 @@ git branch -D branch_name
     |
   develop - M1 D1 
     | 
+origin https://github.com/GoudSagar/automation-cloud.git(fetch)
+
+git remote add upstream https://github.com/devopscloudtrainer/automation-cloud.git
+
+git remote -v
+origin https://github.com/GoudSagar/automation-cloud.git(push)
+origin https://github.com/GoudSagar/automation-cloud.git(fetch)
+upstream https://github.com/devopscloudtrainer/automation-cloud.git(fetch)
+upstream https://github.com/devopscloudtrainer/automation-cloud.git(push)
+
+
+git pull upstream main
+vi transaction.sh -> update -> :wq
+git add .
+git commit -m "Ticket-0993222 - Updated transaction.sh"
+
